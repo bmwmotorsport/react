c43cbe0886a778c1b093a261f18be9a6bc534c52
bmwmotorsport <rkpatel8606@gmail.com>
2025-06-03 13:26:35 +0530

    Update App.js

--- 2_Routing/App.js
+++ 2_Routing/App.js
@@ -4,15 +4,38 @@ import About from './About';
 import Contact from './Contact';
 import Blog from './Blog';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Home/>
+      <Router>
+        <div>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/About">About</Link>
+            </li>
+            <li>
+              <Link to="/Contact">Contact</Link>
+            </li>
+          </ul>
+        </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+
+
+      {/* <Home/>
       <Ser/>
       <About/>
       <Contact/>
-      <Blog/>
+      <Blog/> */}
 
     </div>
   );
